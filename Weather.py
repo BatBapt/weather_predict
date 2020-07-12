7c99f0cb3dd9408f5c75ede86f81d4fe34fe6df8
Baptou <lerouxbaptistepro2@gmail.com>
2020-07-12 23:29:40 +0100

    Application quasi-fonctionnelle

--- Weather.py
+++ Weather.py
@@ -7,7 +7,6 @@ class Weather:
 
     def __init__(self, location):
         self.location = location
-
         self.api_key = self.get_api_key()
         self.get_weather(self.api_key, location)
 
@@ -20,11 +19,11 @@ class Weather:
         """
         This function call the API, take the information about the weather in the city,
         write it in a file (same name of the city) and return a JSON document
-        :param api_key: API key for the call
+        :param key: API key for the call
         :param location: City
         :return: return informations in JSON about the city
         """
-        url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&metric".format(location, key)
+        url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric&lang=fr".format(location, key)
 
         response = requests.get(url).json()
         filename = location + ".txt"
@@ -33,12 +32,15 @@ class Weather:
 
     def extract_component(self, json_file):
         """
-
         :param json_file: JSON file to load the informations
         :return: Not implemented yet
         """
         with open(json_file) as file:
             data = json.load(file)
 
-        temp = data["main"]["temp"] - 273
-        print("Il fait actuellement {} °C".format(temp))
+        clouds = data["weather"][0]["description"]
+        temp = data["main"]["temp"]
+        pressure = data["main"]["pressure"]
+        humidity = data["main"]["humidity"]
+
+        return [clouds, temp, pressure, humidity]
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter.font as tkFont
+
 from Weather import Weather
 
 
@@ -8,31 +10,55 @@ class Application:
         self.root = tk.Tk()
         self.root.title("Météo")
         self.root.geometry("500x500+500+100")
+
+        self.fontStyle = tkFont.Font(family="Lucida Grande", size=15)
+
+        self.frame_city = tk.Frame(self.root, width=500, height=200)
+        self.frame_city.pack()
+        self.info_city = tk.Frame(self.root, width=500, height=500)
+        self.info_city.pack()
+
         self.make_widget()
         self.root.mainloop()
 
         self.entry = None
 
     def make_widget(self):
-        frame_city = tk.Frame(self.root, width=500, height=200)
-        frame_city.pack()
-
-        tk.Label(frame_city, text="Ville").pack()
+        tk.Label(self.frame_city, text="Ville").pack()
 
         city_name = tk.StringVar()
-        self.entry = tk.Entry(frame_city, textvariable=city_name, width=30)
-        self.entry.pack()
+        self.entry = tk.Entry(self.frame_city, textvariable=city_name, width=30)
+        self.entry.pack(padx=10, pady=10)
 
-        weather_btn = tk.Button(frame_city, text="Chercher la météo dans cette ville", command=self.get_weather)
-        weather_btn.pack()
+        weather_btn = tk.Button(self.frame_city, text="Chercher la météo dans cette ville", command=self.get_weather)
+        weather_btn.pack(padx=10, pady=20)
 
         self.root.bind_all("<Return>", self.get_weather)
 
     def get_weather(self, event=None):
+        for widget in self.info_city.winfo_children():
+            widget.destroy()
+
         city_name = self.entry.get()
         if len(city_name) != 0:
             weather = Weather(city_name)
-            weather.extract_component(city_name+".txt")
+            informations = weather.extract_component(city_name+".txt")
+
+            name_label = tk.Label(self.info_city, text="Météo actuelle à " + city_name, font=self.fontStyle)
+            name_label.grid(padx=20, pady=15)
+
+            cloud_label = tk.Label(self.info_city, text="Temps: " + informations[0], font=self.fontStyle)
+            cloud_label.grid(padx=10, pady=10)
+            temp_label = tk.Label(self.info_city, text="Température: " + str(informations[1]), font=self.fontStyle)
+            temp_label.grid(padx=10, pady=10)
+            preassure_label = tk.Label(self.info_city, text="Pression: " + str(informations[2]) + "hPa",
+                                       font=self.fontStyle)
+            preassure_label.grid(padx=10, pady=10)
+            humidity_label = tk.Label(self.info_city, text="Humidité: " + str(informations[3]) + "%",
+                                      font=self.fontStyle)
+            humidity_label.grid(padx=10, pady=10)
+
+        else:
 
 
 
